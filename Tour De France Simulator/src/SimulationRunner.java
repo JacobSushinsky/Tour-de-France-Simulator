276031c11595bd8799945be86f6f7c2fa04b2ed3
jacob.sushinsky <jacob.sushinsky@MU-C1-03.mullen.pvt.k12.co.us>
2023-11-16 09:43:19 -0700

    wrote ride and updateStandings methods; need to clean up details and make it less basic

--- Tour De France Simulator/src/SimulationRunner.java
+++ Tour De France Simulator/src/SimulationRunner.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 public class SimulationRunner
 	{
 		static int playerNumber;
+		static int climbCount;
+		static int sprintCount;
+		static int stageNum;
+		static boolean win;
 		public static void main(String[] args)
 			{
 				Database.fillStartList();
 				Database.fillStageList();
 				Database.fillLeaderboards();
+				createCharacter();
+				int counter = 1;
 				for(Rider r : Database.startList)
 					{
-						System.out.println(r.getName());
+						System.out.println("[" + counter + "] " + r.getName() + " " + r.getTeam());
+						counter ++;
 					}
-				createCharacter();
-//				for(Stage s : Database.stageList)
-//					{
+				for(Stage s : Database.stageList)
+					{
 						introStage();
-//						stageSetUp();
-//						ride();
-//						updateStandings();
-//					}
+						ride();
+						updateStandings();
+					}
 			}
 		public static void createCharacter()
 			{
@@ -35,7 +40,7 @@ public class SimulationRunner
 				int counter = 1;
 				for(Rider r : Database.startList)
 					{
-						System.out.println("[" + counter + "] " + r.getName() + r.getTeam());
+						System.out.println("[" + counter + "] " + r.getName() + " " + r.getTeam());
 						counter ++;
 					}
 				Scanner userInput = new Scanner(System.in);
@@ -55,9 +60,7 @@ public class SimulationRunner
 		public static void introStage()
 			{
 				int stageCount = 1;
-				int stageNum = 0;
-				int climbCount;
-				int sprintCount;
+				stageNum = 0;
 				if(Database.stageList.get(stageNum).getStageType().equals("Mountain"))
 					{
 						climbCount = (int)(Math.random()*3)+3;
@@ -74,5 +77,139 @@ public class SimulationRunner
 						+ " Polka-Dot Jersey Points and " + Database.stageList.get(stageNum).getGreenPoints() + " Green Jersey Points available. Good luck today!");
 				stageCount++;
 				stageNum++;
+			}
+		public static void ride()
+			{
+				int stageCount = stageNum + 1;
+				int gPoints;
+				int fatigue = 0;
+				int teamFatigue = 0;
+				int minutes;
+				int chance;
+				int climbPoints = Database.stageList.get(stageNum).getMountainPoints() / climbCount;
+				if(sprintCount > 0)
+					{
+						gPoints = Database.stageList.get(stageNum).getGreenPoints() / sprintCount;
+					}
+				else
+					{
+						gPoints = Database.stageList.get(stageNum).getGreenPoints();
+					}
+				for(int i = 0; i < climbCount; i++)
+					{
+						Scanner userInput = new Scanner(System.in);
+						System.out.println("You've just made it to the bottom of a climb. There are " + climbPoints + " KOM points up for grabs. Would you like to attack on this climb? y/n");
+						String answer = userInput.nextLine();
+						if(answer.equalsIgnoreCase("y"))
+							{
+								Database.startList.get(playerNumber).setMountainPoints(Database.startList.get(playerNumber).getMountainPoints() + climbPoints);
+								fatigue += 10;
+								System.out.println("You made an attack and though you lost some energy, you won the points! You now have "
+								+ Database.startList.get(playerNumber).getMountainPoints() + " KOM points!");
+							}
+						else
+							{
+								System.out.println("Good strategy! Even though you didn't get any points this time, you saved your strength for the stage win");
+							}
+					}
+				for(int i = 0; i < sprintCount; i++)
+					{
+						Scanner userInput = new Scanner(System.in);
+						System.out.println("You've just made it to the sprint point. there are " + gPoints + " green jersey points up for grabs, and your team is willing to do a leadout."
+								+ " Will you use your leadout team? y/n");
+						String answer = userInput.nextLine();
+						if(answer.equalsIgnoreCase("y"))
+							{
+								Database.startList.get(playerNumber).setSprintPoints(Database.startList.get(playerNumber).getSprintPoints() + gPoints);
+								teamFatigue += 10;
+								System.out.println("You made an attack and though you lost some energy, you won the points! You now have "
+								+ Database.startList.get(playerNumber).getSprintPoints() + " green jersey points!");
+							}
+						else
+							{
+								System.out.println("Good strategy! Even though you didn't get any points this time, you saved your strength for the stage win");
+							}
+					}
+				if(Database.stageList.get(stageNum).getStageType().equals("Mountain"))
+					{
+						System.out.println("You are now towards the end of the stage. Because of the fatigue you've built up during the stage, you will have to dig deep to be able to " + 
+						"win the stage. As you get towards the top of the last climb and the end of the stage, your opponent attacks. Can you go with it?");
+						Scanner userInput = new Scanner(System.in);
+						String answer = userInput.nextLine();
+						if(fatigue > 25)
+							{
+								chance = (int)(Math.random()*6);
+							}
+						else if(fatigue > 15 && fatigue < 25)
+							{
+								chance = (int)(Math.random()*9);
+							}
+						else
+							{
+								chance = (int)(Math.random()*15);
+							}
+						if(chance > 3)
+							{
+								win = true;
+							}
+						else
+							{
+								win = false;
+							}
+						if(win)
+							{
+								minutes = Database.stageList.get(stageNum).getDistance() * 2;
+								System.out.println("Congratulations! You won stage " + stageCount + ", " + Database.stageList.get(stageNum).getStageName() + "! As a result, you" +
+							" earned " + Database.stageList.get(stageNum).getMountainPoints() + " mountain points and took " + minutes + " minutes to complete the stage. Congratulations!");
+							}
+						else
+							{
+								minutes = Database.stageList.get(stageNum).getDistance() * 3;
+								System.out.println("Unfortunately, you built up too much fatigue to be able to cover the opponent's attack. This means that you got no points and are"
+										+ " behind in the yellow jersey standings. Good luck tomorrow, you'll need it!");
+							}
+					}
+			}
+		public static void updateStandings()
+			{
+				int minutes;
+				int seconds;
+				if(win)
+					{
+						minutes = Database.stageList.get(stageNum).getDistance() * 3;
+						seconds = minutes * 60;
+						for(int i = 0; i < Database.startList.size(); i++)
+							{
+								Database.startList.get(i).setTime(Database.startList.get(playerNumber).getTime() + seconds);
+							}
+						minutes = Database.stageList.get(stageNum).getDistance() * 2;
+						seconds = minutes * 60;
+						Database.startList.get(playerNumber).setTime(Database.startList.get(playerNumber).getTime() + seconds);
+					}
+				else
+					{
+						minutes = Database.stageList.get(stageNum).getDistance() * 2;
+						seconds = minutes * 60;
+						for(int i = 0; i < Database.startList.size(); i++)
+							{
+								Database.startList.get(i).setTime(Database.startList.get(playerNumber).getTime() + seconds);
+							}
+						minutes = Database.stageList.get(stageNum).getDistance() * 3;
+						seconds = minutes * 60;
+						Database.startList.get(playerNumber).setTime(Database.startList.get(playerNumber).getTime() + seconds);
+					}
+				int lowestTime = 1000000000;
+				int checkingTime;
+				int leader = 0;
+				for(int i = 0; i < Database.startList.size(); i++)
+					{
+						checkingTime = Database.startList.get(i).getTime();
+						if(checkingTime < lowestTime)
+							{
+								lowestTime = checkingTime;
+								leader = i;
+							}
+					}
+				System.out.println("The leader of the yellow jersey standings at this point is " + Database.startList.get(leader).getName());
 			}
 	}
